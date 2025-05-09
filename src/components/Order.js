e118181fa332b903e52aebea34da993b02791712
ermakgithub <152062275+ermakgithub@users.noreply.github.com>
2025-05-09 03:32:06 +0300

    Fix

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,7 @@ export class Order  extends Component {
     render() {
         return(
             <div className='item'>
-                <img src={"./img/"+this.props.item.img} />
+                <img src={"./img/"+this.props.item.img} alt="Description of the image"/>
                 <h2>{this.props.item.title}</h2>
                 <p>{this.props.item.price}$</p>
                 <CgTrash  className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)}/>
